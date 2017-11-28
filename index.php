e829dbf602fd5e76f427a4ba815f8dc89510c8f8
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-11-28 16:55:31 -0200

    M  index.php

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
-define('ROOT', __DIR__.'/');
-require_once ROOT.'autoload.php';
-require_once ROOT.'vendor/autoload.php';
+define("ROOT", __DIR__."/");
+require_once ROOT."autoload.php";
+require_once ROOT."vendor/autoload.php";
 if (php_sapi_name() == "cli") {
-    $filename=ROOT.'app/.env';
+    $filename=ROOT."app/.env";
     if (file_exists($filename)) {
-        $dotenv = new Dotenv\Dotenv(ROOT.'app');
+        $dotenv = new Dotenv\Dotenv(ROOT."app");
         $dotenv->load();
-        $dbConfig=require_once ROOT.'db.php';
+        $dbConfig=require_once ROOT."db.php";
         $Migration=new Basic\Migration($dbConfig);
     } else {
         die("rename app/example.env to .env");
     }
-} elseif (file_exists(ROOT.'offline') && php_sapi_name() != "cli") {
+} elseif (file_exists(ROOT."offline") && php_sapi_name() != "cli") {
     die("maintenance mode");
-} else {
-    require_once 'app/router.php';
+} elseif (file_exists(ROOT."app/router.php")) {
+    require_once ROOT."app/router.php";
+}else{
+    die("app/router.php not found");
 }
