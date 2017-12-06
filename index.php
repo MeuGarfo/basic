155d87bca97bf1eb41c245b9e57c49d08e33c7dc
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-06 15:50:25 -0200

    M  index.php

--- index.php
+++ index.php
@@ -21,5 +21,5 @@ if (php_sapi_name() == "cli") {
 } elseif (file_exists(ROOT."app/router.php")) {
     require_once ROOT."app/router.php";
 } else {
-    die("sh make 4 && touch app/router.php");
+    die("sh make 4 && touch app/router.php && sh make 1");
 }
