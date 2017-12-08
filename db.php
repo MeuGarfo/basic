91d8b8538064af2c111f43058e436d10f176c7e5
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-12-08 21:02:05 -0200

    M  db.php M  example.env

--- db.php
+++ db.php
@@ -1,34 +1,25 @@
 <?php
-require 'vendor/autoload.php';
-
 if(!defined('ROOT')){
     define('ROOT',__DIR__.'/');
 }
-
-$filename=__DIR__.'/app/.env';
+require ROOT.'vendor/autoload.php';
+$filename=ROOT.'app/.env';
 if (file_exists($filename)) {
     $dotenv = new Dotenv\Dotenv(ROOT.'app');
     $dotenv->load();
-    $dbConfig=[
-        'db_server'=>'localhost',
-        'db_name'=>$_ENV['db_name'],
-        'db_user'=>$_ENV['db_user'],
-        'db_password'=>$_ENV['db_password']
-    ];
-    $Migration=new Basic\Migration($dbConfig);
+    $db=new Medoo\Medoo([
+        'database_type' => 'mysql',
+        'database_name' => $_ENV['db_name'],
+        'server' => $_ENV['db_server'],
+        'username' => $_ENV['db_user'],
+        'password' => $_ENV['db_password']
+    ]);  
+    if ($db) {
+        $Migration=new Basic\Migration($db);
+        return $db;
+    } else {
+        return die($db->error());
+    }     
 } else {
-    die("cp example.env app/.env");
-}
-
-$db=new Medoo\Medoo([
-    'database_type' => 'mysql',
-    'database_name' => $_ENV['db_name'],
-    'server' => 'localhost',
-    'username' => $_ENV['db_user'],
-    'password' => $_ENV['db_password']
-]);
-if ($db) {
-    return $db;
-} else {
-    die($db->error());
+    return die("cp example.env app/.env");
 }
