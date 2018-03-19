b95b8b65d267f4c1198b13003de660a3db1129be
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-03-19 16:05:07 -0300

    M  maintenance.php

--- maintenance.php
+++ maintenance.php
@@ -6,32 +6,24 @@
 </head>
 <body>
     <script type="text/javascript">
-
     function startTimer(duration, display) {
         var timer = duration, minutes, seconds;
         setInterval(function () {
             minutes = parseInt(timer / 60, 10)
             seconds = parseInt(timer % 60, 10);
-
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
-
             display.textContent = minutes + ":" + seconds;
-
             if (--timer < 0) {
-                //timer = duration;
                 window.open(self.location, '_self');
             }
         }, 1000);
     }
-
     window.onload = function () {
         var fiveMinutes = 4,
         display = document.querySelector('#time');
         startTimer(fiveMinutes, display);
     };
-
-    //window.open(self.location, '_self');
 </script>
 <body>
     <div style="text-align:center">
